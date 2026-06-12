d41d26fce04af75b88ee3b472616fd3c93e184d9
LordAfterEight <104775671+LordAfterEight@users.noreply.github.com>
2026-06-12 05:41:56 +0000

    Actually I was stupid. Restore `rax` counter in strlen and remove `r8`

--- src/strlen.asm
+++ src/strlen.asm
@@ -2,13 +2,11 @@ section .text
     global strlen
 
 strlen:         ; Expects str in rsi
-    push r8
-    mov r8, 0
+    mov rax, 0
 .loop:
-    cmp byte [rsi + r8], 0
+    cmp byte [rsi + rax], 0
     je .done
-    inc r8
+    inc rax
     jmp .loop
 .done:
-    pop r8
     ret
